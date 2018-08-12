3128fe688b1a41ff110b525a3ae255f2b3cfcea0
Vincent de Lafontaine <frudup2@hotmail.com>
2018-08-12 13:28:21 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
